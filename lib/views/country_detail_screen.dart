0b23470436c1495facf2effd93cf3de6bda32524
HamzaIqbalMughal <humza.iqbal.hi@gmail.com>
2023-10-30 04:56:12 -0700

    added CountryDetailScreen

---
+++ lib/views/country_detail_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CountryDetailScreen extends StatefulWidget {
+  const CountryDetailScreen({super.key});
+
+  @override
+  State<CountryDetailScreen> createState() => _CountryDetailScreenState();
+}
+
+class _CountryDetailScreenState extends State<CountryDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
